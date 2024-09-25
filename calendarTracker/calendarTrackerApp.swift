23cc4840f879684140a9e0c5213779963ac22740
Clive <cluivee@gmail.com>
2024-09-25 14:51:32 +0100

    Initial Commit

---
+++ calendarTracker/calendarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarTrackerApp.swift
+//  calendarTracker
+//
+//  Created by Clive on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct calendarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
